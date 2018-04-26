520915d2959702dc04e94c2bec89426c39e991f9
joshuaguan <garfilone@gmail.com>
2018-04-26 09:46:52 +0800

    allow admin to create datasource in any org

--- api/datasource.go
+++ api/datasource.go
@@ -41,7 +41,7 @@ func (client *Client) CreateDatasource(datasource *models.Datasource) (*models.D
 }
 
 func (client *Client) DeleteDatasource(datasourceName string) (*models.Message, error) {
-	resp, err:=resty.R().SetHeader(AuthHeader, client.BearerToken).
+	resp, err := resty.R().SetHeader(AuthHeader, client.BearerToken).
 		Delete(client.GrafanaURL + "api/datasources/name/" + datasourceName)
 
 	if err != nil {
@@ -49,6 +49,30 @@ func (client *Client) DeleteDatasource(datasourceName string) (*models.Message, 
 	}
 
 	var message models.Message
+	err = json.Unmarshal(resp.Body(), &message)
+	if err != nil {
+		return nil, err
+	}
+
+	return &message, nil
+}
+
+func (client *Client) AdminCreateDatasource(
+	orgID int,
+	datasource *models.Datasource) (*models.DatasourceSuccessfulCreateMessage, error) {
+	err := client.AdminSwitchOrganization(orgID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := resty.R().SetBasicAuth(client.AdminUser, client.AdminPassword).
+		SetBody(datasource).Post(client.GrafanaURL + "api/datasources")
+
+	if err != nil {
+		return nil, err
+	}
+
+	var message models.DatasourceSuccessfulCreateMessage
 	err = json.Unmarshal(resp.Body(), &message)
 	if err != nil {
 		return nil, err
